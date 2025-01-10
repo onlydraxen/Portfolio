1a7f384c964d8e92ae8f333c2602505a6534070e
onlydraxen <badrbnslm@gmail.com>
2025-01-10 18:43:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+  
+      targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  });
+  
